f2a3444f8352d0044357bdb964214a73768c2461
dongy7 <dongy7@gmail.com>
2017-06-09 22:24:09 -0400

    add header component

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+export default ({ title }) => (
+  <div>
+    <h1>
+      {title}
+    </h1>
+    <style jsx>{`
+      h1 {
+        font-weight: 200;
+        font-size: 32px;
+      }
+    `}</style>
+  </div>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import Header from '../components/Header';
+
 export default () => (
   <div>
-    Welcome to next.js
+    <style jsx global>{`
+      body {
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+      }
+    `}</style>
+    <Header title="Welcome to next.js" />
   </div>
 );
