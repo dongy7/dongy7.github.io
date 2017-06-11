5dfcb9367f4dc47e1c44e80a559635dd6fb5ce5d
dongy7 <dongy7@gmail.com>
2017-06-11 16:50:34 -0400

    moved post component

--- pages/posts.js
+++ pages/posts.js
@@ -1,5 +1,5 @@
+import Link from 'next/prefetch';
 import Page from '../layouts/main';
-import Post from './post';
 import { posts } from '../posts';
 
 export default () =>
@@ -10,3 +10,43 @@ export default () =>
       )}
     </div>
   </Page>;
+
+const Post = ({ id, date, title }) =>
+  <div className="post">
+    <span className="date">{date}</span>
+    <Link href={`/${new Date(date).getFullYear()}/${id}`}>
+      <a>{title} </a>
+    </Link>
+    <style jsx>{`
+      .post {
+        margin-bottom: 10px;
+      }
+
+      .date {
+        display: inline-block;
+        width: 140px;
+        text-align: right;
+        margin-right: 30px;
+        color: #999;
+      }
+
+      a {
+        text-decoration: none;
+      }
+
+      @media (max-width: 500px) {
+        .post {
+          margin-bottom: 15px;
+        }
+
+        .date {
+          display: block;
+          width: inherit;
+          text-align: inherit;
+          font-size: 11px;
+          color: #ccc;
+          margin-bottom: 5px;
+        }
+      }
+    `}</style>
+  </div>;
