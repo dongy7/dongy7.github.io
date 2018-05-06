b4d101bfbb5035cb4ade70766b53f37bc1ac5fa7
Dong Yeop Lee <dongy7@gmail.com>
2018-05-05 21:01:32 -0400

    update about page

--- pages/about.js
+++ pages/about.js
@@ -2,18 +2,17 @@ import Page from '../layouts/Post'
 import Title from '../components/post/Title'
 import P from '../components/post/Paragraph'
 import Link from '../components/post/Link'
+import withMarkdownStyle from '../layouts/withMarkdownStyle'
+import About from './about.md'
 
-export default () =>
-  <Page>
-    <div className="about">
-      <Title>About Me</Title>
-      <P>
-        I'm currently studying Computer Science at the University of North
-        Carolina at Chapel Hill. I'm passionate about open and web technologies.
-        Feel free to reach out to <Link to={'mailto:dongy7@gmail.com'}>
-          me
-        </Link>{' '}
-        if you are interested in working with me.
-      </P>
-    </div>
-  </Page>
+export default () => {
+  const Content = withMarkdownStyle(About)
+  return (
+    <Page>
+      <div className="about">
+        <Title>About Me</Title>
+        <Content />
+      </div>
+    </Page>
+  )
+}
---
+++ pages/about.md
@@ -0,0 +1,3 @@
+I'm currently studying Computer Science at the University of North
+Carolina at Chapel Hill. In my spare time I like to hack on projects with open source technologies. If you find any errors in my posts or just want to reach out, feel free
+to contact [me](mailto:dong@dongyeop.com).
