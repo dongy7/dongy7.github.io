863b7236d837d0fa74e6935f9cbcfb9b1d441941
Dong Yeop Lee <dongy7@gmail.com>
2018-05-15 11:45:44 -0400

    update menu icon

--- components/nav/Arrow.js
+++ components/nav/Arrow.js
@@ -17,23 +17,7 @@ const Menu = styled.div`
     transform: ${props => (props.toggled ? 'rotate(180deg)' : '')};
   }
 `
-
 export default ({ onClick, toggled }) =>
   <Menu toggled={toggled}>
-    <svg
-      width="27px"
-      height="14px"
-      viewBox="0 0 27 14"
-      version="1.1"
-      style={{ fill: 'inherit' }}
-      onClick={() => onClick()}
-    >
-      <g id="Page-1" stroke="none" strokeWidth="1" fillRule="evenodd">
-        <polygon
-          id="Arrow"
-          fillRule="nonzero"
-          points="13.4999996 13.9214282 0.6 1.17499997 1.82857139 0.1 13.4999996 11.7714282 25.1714278 0.1 26.3999992 1.17499997"
-        />
-      </g>
-    </svg>
+    <i class="fas fa-lg fa-chevron-down" onClick={() => onClick()}></i>
   </Menu>
