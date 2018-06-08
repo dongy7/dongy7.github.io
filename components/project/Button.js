9a039c3fa587dac7c619c294dde0c254d45f5091
Dong Yeop Lee <dongy7@gmail.com>
2018-06-08 10:24:04 -0400

    add button hover styling

--- components/project/Button.js
+++ components/project/Button.js
@@ -1,15 +1,25 @@
 import styled from 'styled-components'
 
+const primaryColor = '#3369e7'
+const secondaryColor = '#ff4f81'
+
 const Button = styled.a.attrs({
   target: '_blank'
-}) `
+})`
   border-radius: 3px;
   padding: 0.25em 1em;
   margin: 0 0.5em;
-  margin-left: ${props => props.first ? 0 : '0.5em'};
+  margin-left: ${props => (props.first ? 0 : '0.5em')};
   background: transparent;
-  color: #bd10e0;
-  border: 2px solid #bd10e0;
-`;
+  color: ${props => (props.primary ? primaryColor : secondaryColor)};
+  border: 2px solid;
+  border-color: ${props => (props.primary ? primaryColor : secondaryColor)};
+
+  &:hover {
+    background-color: ${props =>
+      props.primary ? primaryColor : secondaryColor};
+    color: white;
+  }
+`
 
 export default Button
