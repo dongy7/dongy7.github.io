6cc20ee2595553d8d755ca157b5e4a9add8b513c
Dong Yeop Lee <dongy7@gmail.com>
2018-06-08 10:45:16 -0400

    remove path map

--- next.config.js
+++ next.config.js
@@ -8,25 +8,13 @@ const config = {
       use: [
         defaultLoaders.babel,
         {
-          loader: '@mdx-js/loader'
-        }
-      ]
+          loader: '@mdx-js/loader',
+        },
+      ],
     })
 
     return config
   },
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      '/posts': { page: '/posts' },
-      '/projects': { page: '/projects' },
-      '/terminal': { page: '/terminal' },
-      '/2017/fun-with-circuits': { page: '/2017/fun-with-circuits' },
-      '/2017/complex-data-fetching-made-simple-with-rx': { page: '/2017/complex-data-fetching-made-simple-with-rx' },
-      '/2018/integrating-markdown-in-react-with-mdx': { page: '/2018/integrating-markdown-in-react-with-mdx' },
-    }
-  }
 }
 
 module.exports = withCSS(config)
