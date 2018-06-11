b5059ca67a863e9fc26eb748f109af6ce9a62793
Dong Yeop Lee <dongy7@gmail.com>
2018-06-11 09:26:25 -0400

    New build on 2018-06-11T13:26:25.241Z.

--- _next/static/commons/manifest.js
+++ _next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "93720588f3b4e8a9e076"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "e54f906665d50bb5c2df"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_document.js","2":"bundles/pages/_error.js","3":"bundles/pages/_app.js","5":"bundles/pages/index.js","6":"bundles/pages/projects.js"}[chunkId]||chunkId) + "-" + {"0":"f7af72de4777739481af","1":"cec14f900fec332aa2b2","2":"99988b845fbb92323b21","3":"623caac567f9b1515e77","5":"17c4f6280db606dcce7b","6":"6ebeaa6dbefa4118b0ce"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_document.js","2":"bundles/pages/_error.js","3":"bundles/pages/_app.js","5":"bundles/pages/index.js","6":"bundles/pages/projects.js"}[chunkId]||chunkId) + "-" + {"0":"82b4b98c44e517882a43","1":"cec14f900fec332aa2b2","2":"99988b845fbb92323b21","3":"623caac567f9b1515e77","5":"17c4f6280db606dcce7b","6":"324387129832b7d70ef3"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
